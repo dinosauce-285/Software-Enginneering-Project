f86b2eb34330b13408f61142851789c3a980f877
dinosauce-285 <thanhbualqt@gmail.com>
2025-07-14 22:44:58 +0700

    up log in site

--- src/frontend/src/components/mediaButton.jsx
+++ src/frontend/src/components/mediaButton.jsx
@@ -1,7 +1,7 @@
 function MediaButton({children})
 {
     return (
-        <button className="bg-black text-white h-[30%] w-[20rem] rounded-[2rem] flex flex-row justify-center items-center mb-[2%]">{children}</button>
+        <button className="bg-black text-white h-[3rem] w-[20rem] rounded-[2rem] flex flex-row justify-center items-center mb-[2%]">{children}</button>
     )
 }
 export default MediaButton;
